b63c18859371197d553ef073dddb12e24c99c571
Jason Malefakis <anskyny10@aol.com>
2024-06-16 18:10:38 -0400

    update cats array

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+//name = "Ralph"
+function destructivelyAppendCat(name) {
+    cats.push(name)
+    return cats;
+}
+
+//name = "Garfield"
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+    return cats;
+}
+
+//remove last
+function destructivelyRemoveLastCat() {
+    cats.pop()
+    return cats;
+}
+
+//remove first
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+    return cats;
+}
+
+//append
+function appendCat(name) {
+    const newCats = [...cats, name]
+    return newCats;
+}
+
+//prepend
+function prependCat(name) {
+    const newCats = [name, ...cats]
+    return newCats;
+}
+
+//remove last
+function removeLastCat() {
+    const newCats = cats.slice(0, -1)
+    return newCats;
+}
+
+//remove first
+function removeFirstCat() {
+    const newCats = cats.slice(1)
+    return newCats;
+}
